2bee9181cf88c27d0f2441a4efae4fb9a6286c89
Les Chinyanga <les.chinyanga@gmail.com>
2024-07-19 16:48:59 +0200

    feat(change-player): add change player function

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import "./AppStyles.css";
 
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(""));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  const changePlayer = () => {
+    currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X");
+  };
 
   useEffect(() => {
     console.log(squares);
   }, []);
   return (
     <>
+      <p>Current Player is: {currentPlayer}</p>
       <div className="board">
         {squares.map((square, index) => {
           return (
-            <button className="square" key={index}>
+            <button onClick={changePlayer} className="square" key={index}>
               {index}
             </button>
           );
