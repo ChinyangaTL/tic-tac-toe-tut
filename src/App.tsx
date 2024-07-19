2df13863c017e7cf177b64d7adaffbb8e35f8dde
Les Chinyanga <les.chinyanga@gmail.com>
2024-07-19 16:41:55 +0200

    feat(board): render board to screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./AppStyles.css";
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+
+  useEffect(() => {
+    console.log(squares);
+  }, []);
   return (
     <>
-      <div>hi</div>
+      <div className="board">
+        {squares.map((square, index) => {
+          return (
+            <button className="square" key={index}>
+              {index}
+            </button>
+          );
+        })}
+      </div>
     </>
   );
 }
